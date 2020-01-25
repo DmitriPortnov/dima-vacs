a0c5f0224fbb82ebc5a5764977a6618536ae0747
Dmitri Portnov <Dmitriportnov89@gmail.com>
2020-01-25 18:15:05 +0200

    front

---
+++ public/precache-manifest.ad624d19d8371fa7c6f8c6404c976c92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa1bdf317039be7d6db0ecd36d5794a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81b2cdb2e91bceb8797f",
+    "url": "/static/css/main.c249f731.chunk.css"
+  },
+  {
+    "revision": "5f5f4bfbfdc307b15db4",
+    "url": "/static/js/2.a4f7a52c.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/static/js/2.a4f7a52c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "81b2cdb2e91bceb8797f",
+    "url": "/static/js/main.b5481daf.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  },
+  {
+    "revision": "d1af5f6f736f4c4c556a17a77d689629",
+    "url": "/static/media/main-background.d1af5f6f.jpg"
+  }
+]);
