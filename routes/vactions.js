b300f244867f1c2a9100d7e1d8ef9abce2255a6f
Dmitri Portnov <Dmitriportnov89@gmail.com>
2020-01-25 18:24:28 +0200

    adding authentication to the API routes

--- routes/vactions.js
+++ routes/vactions.js
@@ -2,13 +2,14 @@ const express = require('express');
 
 const service = require('../services/vacations.service');
 const validation = require('../validations/vacation.validation');
+const auth = require('../validations/user.validation');
 
 const router = express.Router();
 
-router.get('/', service.getAllVacations);
+router.get('/', auth.loggedIn, service.getAllVacations);
 router.get('/vacImg/:imgId', service.getVacationImage);
-router.put('/', validation.checkInputs, validation.checkFile, service.createNewVacation);
-router.patch("/:vacId", validation.checkInputs, service.updateVacation);
-router.delete("/:vacId", service.deleteVacation);
+router.put('/', auth.loggedIn, validation.checkInputs, validation.checkFile, service.createNewVacation);
+router.patch("/:vacId", auth.loggedIn, validation.checkInputs, service.updateVacation);
+router.delete("/:vacId", auth.loggedIn, service.deleteVacation);
 
 module.exports = router;
